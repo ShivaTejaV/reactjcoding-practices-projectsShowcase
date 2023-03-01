75394dd3c285c474737d664f927a0f9c001c2152
ShivaTejaV <vddshivateja@gmail.com>
2023-03-01 18:27:06 +0000

    first

---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Nav = () => (
+  <nav className="nav-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/projects-showcase/website-logo-img.png "
+      alt="website logo"
+      className="website-logo"
+    />
+  </nav>
+)
+
+export default Nav
